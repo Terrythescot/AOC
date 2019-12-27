1e2a737af8c24937df2f21d3c24164253a820aab
Terry <tpgauld@gmail.com>
2019-12-27 11:53:49 +0000

    ex1part2

--- ex1.rb
+++ ex1.rb
@@ -2,7 +2,14 @@ def fuel_for_mass(mass)
   (mass / 3) - 2
 end
 
+def recursive_fuel_for_mass(mass)
+  return 0 if mass <= 5
+
+  fuel = (mass / 3) - 2
+  fuel + recursive_fuel_for_mass(fuel)
+end
+
 puts File.read('x1.data')
          .split("\n")
-         .map { |i| fuel_for_mass i.to_i }
+         .map { |i| recursive_fuel_for_mass i.to_i }
          .sum
